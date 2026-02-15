f02bafe5634f618e156827ee5da9d457f86f1eba
elidotjs <elidotpy@gmail.com>
2026-02-14 21:46:57 -0300

    i dont know. do NOT ask me.

--- src/bin/killcompiler/rusty.rs
+++ src/bin/killcompiler/rusty.rs
@@ -4,25 +4,55 @@
 // PIPELINE: Validate structure -> Compress into Instructions
 // EXECUTE: instructionize(Vec<Token>)
 //
-
+//
+#![allow(dead_code)]
+use crate::toki::{Token, Types};
 use std::sync::LazyLock;
 
-static TABLE: LazyLock<Vec<Vec<Token>>> =
-    LazyLock::new(|| vec![vec![Token::Text(String::from("var"))]]);
+static TABLE: LazyLock<Vec<(Vec<Token>, GenericInstructions)>> = LazyLock::new(|| {
+    vec![
+        (
+            vec![
+                Token::Text(String::from("var")),
+                Token::Generic(Types::Text),
+                Token::Symbol('='),
+                Token::Generic(Types::Any),
+                Token::Symbol(';'),
+            ],
+            GenericInstructions::VariableAssignment,
+        ),
+        (
+            vec![
+                Token::Generic(Types::Text),
+                Token::Symbol('='),
+                Token::Generic(Types::Any),
+                Token::Symbol(';'),
+            ],
+            GenericInstructions::VariableReassignment,
+        ),
+    ]
+});
 
-use crate::toki::Token;
-
+#[derive(Debug)]
 enum Instructions {
-    VariableAssignment(Token),
+    VariableAssignment(String, Token),
 }
 
-fn starts_with(tokens: &Vec<Token>, with_what: Vec<Token>) -> bool {
+enum GenericInstructions {
+    VariableAssignment,
+    VariableReassignment,
+}
+
+fn starts_with(tokens: &[Token], with_what: &[Token]) -> bool {
     if tokens.len() < with_what.len() {
         return false;
     }
 
+    println!("{:?}, {:?}", tokens, with_what);
+
     for (index, token) in tokens.iter().enumerate() {
         let with_what_item = with_what.get(index).unwrap();
+        println!("{:?} ({:?})", with_what_item, token);
 
         token.equals_to(with_what_item).unwrap();
     }
@@ -33,16 +63,50 @@ fn starts_with(tokens: &Vec<Token>, with_what: Vec<Token>) -> bool {
 pub fn instructionize(tokens: Vec<Token>) {
     let mut lines = Vec::new();
     let mut current_line = Vec::new();
+    let mut instructions_vec: Vec<Instructions> = Vec::new();
 
     for token in tokens {
+        current_line.push(token.clone());
+
         if token == Token::Symbol(';') {
             lines.push(current_line.clone());
             current_line.clear();
             continue;
         }
-
-        current_line.push(token);
     }
 
-    println!("{:?}", lines)
+    for line in lines {
+        for (potential_match, instruction) in TABLE.iter() {
+            if !starts_with(&line, potential_match) {
+                continue;
+            }
+
+            instructions_vec.push(match instruction {
+                GenericInstructions::VariableAssignment => {
+                    let name;
+
+                    if let Token::Text(value) = line.get(1).unwrap() {
+                        name = value;
+                    } else {
+                        panic!("what")
+                    }
+
+                    Instructions::VariableAssignment(name.to_owned(), line.get(3).unwrap().clone())
+                }
+                GenericInstructions::VariableReassignment => {
+                    let name;
+
+                    if let Token::Text(value) = line.first().unwrap() {
+                        name = value;
+                    } else {
+                        panic!("what")
+                    }
+
+                    Instructions::VariableAssignment(name.to_owned(), line.get(2).unwrap().clone())
+                }
+            })
+        }
+    }
+
+    println!("{:?}", instructions_vec)
 }
--- src/bin/killcompiler/toki.rs
+++ src/bin/killcompiler/toki.rs
@@ -1,4 +1,4 @@
-use std::io::Error;
+use std::vec::IntoIter;
 
 //
 // CAT: Toki
@@ -14,6 +14,7 @@ pub enum Types {
     Text,
     Number,
     Symbol,
+    Any,
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -24,14 +25,11 @@ pub enum Token {
     Generic(Types),
 }
 
-pub enum BlueprintToken {
-    Text,
-    Number,
-    Symbol,
-}
-
 impl Token {
     pub fn equals_to(&self, other: &Self) -> Result<bool, &'static str> {
+        if other == &Token::Generic(Types::Any) {
+            return Ok(true);
+        }
         match self {
             Token::Text(_) => Ok(self == other || other == &Token::Generic(Types::Text)),
             Token::Number(_) => Ok(self == other || other == &Token::Generic(Types::Number)),
@@ -94,6 +92,7 @@ fn separate_into_chunks(string: String) -> Vec<String> {
     if !current_token.trim().is_empty() {
         chunks.push(current_token);
     }
+
     chunks
 }
 
@@ -101,7 +100,7 @@ fn separate_into_chunks(string: String) -> Vec<String> {
 /// (also see separate_into_chunks)
 /// ["var", "thing", "=", "10", ";"] -> [Text("var"), Text("thing"), Symbol("="), Number("10"), Symbol(";")]
 ///
-fn compress_into_raw_tokens(chunks: Vec<String>) -> Vec<Token> {
+fn compress_into_raw_tokens<'a>(chunks: Vec<String>) -> IntoIter<Token> {
     let text_regex = REGEXES.get("text").unwrap();
     let number_regex = REGEXES.get("number").unwrap();
 
@@ -113,28 +112,30 @@ fn compress_into_raw_tokens(chunks: Vec<String>) -> Vec<Token> {
         } else if text_regex.is_match(&chunk) {
             tokens.push(Token::Text(chunk));
         } else {
-            tokens.push(Token::Symbol(chunk.chars().nth(0).unwrap()))
+            tokens.push(Token::Symbol(chunk.chars().next().unwrap()))
         }
     }
 
-    tokens
+    tokens.into_iter()
 }
 
-pub fn tokenize(text: String) -> Vec<Token> {
-    compress_into_raw_tokens(separate_into_chunks(text))
+pub fn tokenize<'a>(text: String) -> Vec<Token> {
+    let raw_tokens = compress_into_raw_tokens(separate_into_chunks(text));
+
+    raw_tokens.collect()
 }
 
 #[test]
 fn test_raw_equals() {
     assert!(
-        Token::Number(String::from("34.2"))
-            .equals_to(&Token::Number(String::from("34.2")))
+        Token::Number("34.2".to_string())
+            .equals_to(&Token::Number("34.2".to_string()))
             .unwrap()
     );
 
     assert!(
-        !Token::Number(String::from("34.2"))
-            .equals_to(&Token::Number(String::from("34.3")))
+        !Token::Number("34.2".to_string())
+            .equals_to(&Token::Number("34.3".to_string()))
             .unwrap()
     );
 }
@@ -142,13 +143,13 @@ fn test_raw_equals() {
 #[test]
 fn test_generic_equals() {
     assert!(
-        Token::Number(String::from("34.2"))
+        Token::Number("34.2".to_string())
             .equals_to(&Token::Generic(Types::Number))
             .unwrap()
     );
 
     assert!(
-        !Token::Number(String::from("34.2"))
+        !Token::Number("34.2".to_string())
             .equals_to(&Token::Generic(Types::Text))
             .unwrap()
     );
